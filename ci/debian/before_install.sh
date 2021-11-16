a024b49ec10f8754a530f425e7e286260e6e43b2
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2021-11-16 17:56:39 +0100

    fix sonar

--- ci/debian/before_install.sh
+++ ci/debian/before_install.sh
@@ -15,8 +15,6 @@ wget https://github.com/CGAL/cgal/releases/download/v"$2"/CGAL-"$2".tar.xz
 tar xJf CGAL-"$2".tar.xz
 cd CGAL-"$2" ;
 mkdir build ;
-cd build ;
-cmake -DCMAKE_INSTALL_PREFIX=$1/CGAL .. ;
-make ;
-make install ;
-cd ../..
+cmake  .. ;
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX=$1/CGAL ;
+cmake --build build/ --config Release
