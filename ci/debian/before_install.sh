3b1f77108968d86d858f5f5d198b8a62fa8361a4
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2021-11-17 07:02:11 +0100

    remove cmake ..

--- ci/debian/before_install.sh
+++ ci/debian/before_install.sh
@@ -15,6 +15,5 @@ wget https://github.com/CGAL/cgal/releases/download/v"$2"/CGAL-"$2".tar.xz
 tar xJf CGAL-"$2".tar.xz
 cd CGAL-"$2" ;
 mkdir build ;
-cmake  .. ;
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX=$1/CGAL ;
 cmake --build build/ --target install --config Release
